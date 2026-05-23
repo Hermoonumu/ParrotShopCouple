fd791765a5eae1f118b2e188c77898fbaef44909
Herman <geron9988@gmail.com>
2026-05-23 16:24:46 +0200

    Fix nested folders and upload actual files

---
+++ ParrotShopBackend/Migrations/20260522235706_akusernametoindex.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParrotShopBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class akusernametoindex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Users_Username",
+                table: "Users");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Username",
+                table: "Users");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Users_Username",
+                table: "Users",
+                column: "Username");
+        }
+    }
+}
